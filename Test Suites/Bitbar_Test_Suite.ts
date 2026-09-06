9e8906e10bdb5d6996e3ba37fb1348be1af4ff43
LNU <ganeshxlnu@deloitte.ca>
2026-09-07 02:18:10 +0530

    Update renamed kayalon project file

---
+++ Test Suites/Bitbar_Test_Suite.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Bitbar_Test_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <maxConcurrentInstances>1</maxConcurrentInstances>
+   <numberOfRerun>3</numberOfRerun>
+   <orchestration>CLASSIC</orchestration>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f79e07b1-fe7b-4b90-84a8-f5f65bf93f6c</testSuiteGuid>
+   <testCaseLink>
+      <guid>158a6188-3083-49c9-b001-daeeebe008d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bitbar_sample_url_test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
